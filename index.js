d4c4a11b123120c7e5362c7c6286a02a354bf191
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-01-02 14:56:05 -0600

    first commit ,  install and configure  node

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+
+//Definir puerto
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => { 
+    console.log( `El servidor esta funcionando en el puerto ${port}` )
+ })
