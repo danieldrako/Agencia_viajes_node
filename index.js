187443849c96968e30497fa5b3490221cabe668c
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-01-04 22:14:02 -0600

    after Enabling the Import and Export Syntax

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express');
+import express  from 'express';
 
 const app = express();
 
 
 //Definir puerto
 const port = process.env.PORT || 9000;
+
+
+app.get('/', (req,res) => { //req- lo que enviamos; res - lo que express nos responde
+    res.render('Inicio')
+ })
+app.get('/nosotros', (req,res) => { //req- lo que enviamos; res - lo que express nos responde
+    res.render('Nosotros')
+ })
+app.get('/contacto', (req,res) => { //req- lo que enviamos; res - lo que express nos responde
+    res.render('Contacto')
+ })
 
 app.listen(port, () => { 
     console.log( `El servidor esta funcionando en el puerto ${port}` )
