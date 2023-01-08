2918ab80ca3aeef69362ded6c57f6dcc76d6bb9c
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-01-07 22:35:21 -0600

    after variables to the views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express  from 'express';
 const router = express.Router();
 
 router.get('/', (req,res) => { //req- lo que enviamos; res - lo que express nos responde
-    res.send('Inicio')
+    res.render('inicio')
  })
 router.get('/nosotros', (req,res) => { //req- lo que enviamos; res - lo que express nos responde
     res.render('nosotros')
